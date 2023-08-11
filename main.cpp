d0b1de2581c01e5eb128a4feced6a939083f6437
Barış Anılır <b21827043@cs.hacettepe.edu.tr>
2023-08-11 11:44:48 +0300

    update2

--- main.cpp
+++ main.cpp
@@ -9,34 +9,51 @@ using namespace cv;
 
 int main() {
     // Video dosyasını yükle
-    cv::VideoCapture cap("v4l2src device=/dev/video1 ! image/jpeg, framerate=30/1,width=640,height=480,format=(string)MJPG ! jpegdec ! videoconvert ! appsink");
+    cv::VideoCapture cap("v4l2src device=/dev/video0 ! image/jpeg, framerate=30/1,width=1280,height=720,format=(string)MJPG ! jpegdec ! videoconvert ! appsink");
     if (!cap.isOpened()) {
         std::cout << "Video dosyası yüklenemedi!" << std::endl;
         return -1;
     }
-
+    
     // İlk kareyi oku
     cv::Mat frame;
     cap >> frame;
+
+    int width = frame.cols;
+    int height = frame.rows;
+    int k = 4;
     cv::namedWindow("Nesne Takibi");
 
     // İzlenecek nesneyi seçin
-    cv::Rect boundingBox = cv::selectROI("Nesne Takibi",frame, false);
+    cv::Rect2d boundingBox = cv::selectROI("Nesne Takibi",frame, false);
+    boundingBox.x = boundingBox.x/4;
+    boundingBox.y = boundingBox.y/4;
+    boundingBox.width = boundingBox.width/4;
+    boundingBox.height = boundingBox.height/4;
 
     // CSRT takipçi nesnesini oluştur
     Ptr<Tracker> tracker = TrackerCSRT::create();
+    cv::resize(frame, frame, cv::Size(width/k, height/k));
 
     // Nesneyi takip etmek için başlangıç noktasını ve boyutunu ayarla
     tracker->init(frame, boundingBox);
 
     // Videoyu oynat ve nesneyi takip et
-    while (cap.read(frame)) {
+    while (true) {
         // Başlangıç zamanını kaydet
+        cap >> frame;
         auto start = std::chrono::high_resolution_clock::now();
 
+        cv::resize(frame, frame, cv::Size(width/4, height/4));
+        
         // Nesneyi güncelle
+        
         bool success = tracker->update(frame, boundingBox);
-
+        boundingBox.x = boundingBox.x*4;
+        boundingBox.y = boundingBox.y*4;
+        boundingBox.width = boundingBox.width*4;
+        boundingBox.height = boundingBox.height*4;
+        cv::resize(frame, frame, cv::Size(width, height));
         // Bitiş zamanını kaydet
         auto end = std::chrono::high_resolution_clock::now();
         
